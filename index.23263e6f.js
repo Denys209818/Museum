251922a907bb8c9286088436f974ff2982b6a5b6
Denys209818 <deniskravcuk63@gmail.com>
2023-11-29 12:02:21 +0200

    make build

---
+++ index.23263e6f.js
@@ -0,0 +1,3 @@
+document.getElementById("email-form").addEventListener("submit",function(e){e.preventDefault(),e.stopPropagation(),document.getElementsByClassName("main__nav-email")[0].value=""});//# sourceMappingURL=index.23263e6f.js.map
+
+//# sourceMappingURL=index.23263e6f.js.map
